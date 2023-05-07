c7410d497957f68b9804805c77f2d3c2185e0c1e
revecen <m.poplavskij@gmail.com>
2023-05-07 11:55:32 +0300

    ABOBA2

--- src/pages/Posters.js
+++ src/pages/Posters.js
@@ -1,5 +1,11 @@
 import React, {Component} from 'react';
 
+function swap()
+{
+    return React.cloneElement(this.doRender(),{
+        style: {display: 'inline'},
+})
+}
 class Posters extends Component {
     render() {
         return (
@@ -7,10 +13,27 @@ class Posters extends Component {
                 <div className={"App-previewPosters"}></div>
                 <div className={"App-Sidebar"}>
                     <div className={"App-SidebarText"}>
-                        Тематика
+                        <table>
+                            <tr>
+                            Тематика
+                            <img className={"App-SidebarIconPlus"} src={require("../images/Vector.png")} alt={"plus"} id={"plus"} onClick={swap}/>
+                                <img className={"App-SidebarIconMinus"} src={require("../images/Vectorminus.png")} alt={"minus"} id={"minus"} onClick={swap} />
+                            </tr>
+                        </table>
                     </div>
                     <div className={"App-SidebarText"}>
-                        Тематика
+                        <table>
+                            <tr>
+                            Форма
+                            </tr>
+                        </table>
+                    </div>
+                </div>
+                <div className={"App-Market"}>
+                    <div className={"App-Poster"}>
+                        <img src={require("../images/FirstPoster.png")} alt={"Love"}/>
+                        <span className={"App-PosterText"}>Love</span>
+                        <span className={"App-Price"}> 150 грн</span>
                     </div>
                 </div>
             </div>
